77255e3578765e930899bef1fae237ee7fd9d64a
Peter Naimoli <pnaimoli@users.noreply.github.com>
2023-12-13 19:21:04 -0600

    Update build.sh file

--- build.sh
+++ build.sh
@@ -12,4 +12,6 @@ cd ..
 mkdir -p build/client
 rm -rf build/client/*
 cp -r client/build/* build/client/
-cp -p server/{chordmania.py,requirements.txt,wsgi.py} build/
+mkdir -p build/chordmania
+cp -p server/{xmlserver.py,requirements.txt,wsgi.py} build/
+cp -p server/chordmania/*.py build/chordmania
