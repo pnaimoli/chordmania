6a9c64200b00ca634feeb987740bfe97fe56cf2a
Peter Naimoli <pnaimoli@users.noreply.github.com>
2023-12-13 19:44:55 -0600

    xmlserver actually works now.

--- server/xmlserver.py
+++ server/xmlserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, send_from_directory
 import os
 
 import music21
@@ -18,7 +18,7 @@ def serve(path):
     <body>
         <h1>Welcome to ChordMania</h1>
         <p>This page is under construction.
-        Check out <a href="./xmlgen">XMLGen</a>.</p>
+        Check out <a href="./xmlgen?notes=4&measures=3">XMLGen</a>.</p>
     </body>
     </html>
     '''
@@ -27,13 +27,22 @@ def serve(path):
 #    else:
 #        return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/xmlgen', methods=['POST'])
+@app.route('/xmlgen', methods=['GET'])
 @app.route('/xmlgen')
-def process_image():
-#    image_data_url = request.json.get('imageData')
-#    header, encoded = image_data_url.split(",", 1)
-    cg = chordmania.CMChordGenerator(4, 10, music21.key.Key('E'), both_hands=False)
-    return jsonify(cg.get_xml())
+def generate_xml():
+    # Default values
+    notes_per_chord = 4
+    num_chords = 10
+    key_signature = 'E'
+    both_hands = False
+
+    notes_per_chord = request.args.get('notes', default=notes_per_chord, type=int)
+    num_chords = request.args.get('measures', default=num_chords, type=int)
+    key_signature = request.args.get('key', default=key_signature, type=str)
+    both_hands = request.args.get('both_hands', default=both_hands, type=lambda x: x.lower() == 'true')
+
+    cg = chordmania.CMChordGenerator(notes_per_chord, num_chords, music21.key.Key(key_signature), both_hands)
+    return cg.get_xml(), 200, {'Content-Type': 'application/xml'}
 
 if __name__ == '__main__':
-    app.run('localhost',4999,debug=True)
+    app.run('localhost', 4999, debug=True)
